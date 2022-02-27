539dc7affe813906699696d1534738d7f383134d
SergeiPospelov <pospelka413@gmail.com>
2022-02-27 16:34:57 +0300

    added Homework1 with task2

---
+++ Homework1/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = new int();
+int b = new int();
+
+Console.Write("Введите число a: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(a);
+    Console.Write("Min = ");
+    Console.Write(b);
+}
+
+else 
+{
+    Console.Write("Max = ");
+    Console.WriteLine(b);
+    Console.Write("Min = ");
+    Console.Write(a); 
+}
