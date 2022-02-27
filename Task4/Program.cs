9ad5cc944b9214819ccddfb552707af4e8cff9ac
SergeiPospelov <pospelka413@gmail.com>
2022-02-27 15:34:09 +0300

    added tasks from lesson1

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿int SquareNumber(int N)
+{
+    int result = N * N;
+    return result;
+}
+
+int Number1 = 4;
+int Number2 = -3;
+int Number3 = -7;
+Console.WriteLine(SquareNumber(Number1));
+Console.WriteLine(SquareNumber(Number2));
+Console.WriteLine(SquareNumber(Number3));
