c4962b348724841e40d8835116acc384d287594f
SergeiPospelov <pospelka413@gmail.com>
2022-02-27 17:39:25 +0300

    added Task6

---
+++ Homework1/Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = new int();
+int divisionRemainder = new int();
+
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+divisionRemainder = number % 2; //Получаем остаток от деления на 2
+
+if (divisionRemainder == 0)
+{
+    Console.Write("Число чётное");
+}
+
+else 
+{
+    Console.Write("Число нечётное");
+}
