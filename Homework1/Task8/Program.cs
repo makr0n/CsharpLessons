ee5ea8dc07f36a59ea9834ff39d664059a22c230
SergeiPospelov <pospelka413@gmail.com>
2022-02-27 17:54:59 +0300

    added Task8

---
+++ Homework1/Task8/Program.cs
@@ -0,0 +1,21 @@
+﻿int N = new int();
+int divisionRemainder = new int();
+int count = new int();
+
+Console.WriteLine("Введите число: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+count = 1;
+
+while (count <= N)
+{
+    divisionRemainder = count % 2;
+
+    if (divisionRemainder == 0)
+    {
+        Console.Write(count);
+        Console.Write(" ");
+    }
+    
+    count++;
+}
