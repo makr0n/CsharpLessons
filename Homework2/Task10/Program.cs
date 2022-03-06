13e16ee144808f6882883d34c1ee66c0971af543
SergeiPospelov <pospelka413@gmail.com>
2022-03-06 18:59:16 +0300

    Added second homework with task10

---
+++ Homework2/Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = new int();
+int secondDigit = new int();
+
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+secondDigit = number / 10; //Отбрасываем первый разряд числа
+secondDigit = secondDigit % 10; // Получаем значение второго разряда
+
+Console.Write($"{secondDigit} является второй цифрой числа {number}");
