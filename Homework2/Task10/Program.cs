63fcb186d7960fefd04abb760c239b5a76f661f6
SergeiPospelov <pospelka413@gmail.com>
2022-03-06 20:18:21 +0300

    Added Task13

--- Homework2/Task10/Program.cs
+++ Homework2/Task10/Program.cs
@@ -1,4 +1,6 @@
-﻿int number = new int();
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int number = new int();
 int secondDigit = new int();
 
 Console.Write("Введите число: ");
@@ -7,4 +9,4 @@ number = Convert.ToInt32(Console.ReadLine());
 secondDigit = number / 10; //Отбрасываем первый разряд числа
 secondDigit = secondDigit % 10; // Получаем значение второго разряда
 
-Console.Write($"{secondDigit} является второй цифрой числа {number}");
+Console.Write($"Цифра {secondDigit} является второй цифрой числа {number}");
---
+++ Homework2/Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int number = new int();
+int numberForDisplay = new int();
+int thirdDigit = new int();
+
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+numberForDisplay = number;
+
+if (number < 99)
+{
+    Console.Write("Третьего числа нет");
+}
+
+else
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    thirdDigit = number % 10;
+    
+    Console.Write($"Цифра {thirdDigit} является третьей цифрой числа {numberForDisplay}");
+}
+
