6a363f59648192952857bb3ec7fbd0f4616dab56
SergeiPospelov <pospelka413@gmail.com>
2022-03-06 17:46:03 +0300

    changed variable names in Task2

--- Homework1/Task2/Program.cs
+++ Homework1/Task2/Program.cs
@@ -1,23 +1,23 @@
-﻿int a = new int();
-int b = new int();
+﻿int number1 = new int();
+int number2 = new int();
 
 Console.Write("Введите число a: ");
-a = Convert.ToInt32(Console.ReadLine());
+number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число b: ");
-b = Convert.ToInt32(Console.ReadLine());
+number2 = Convert.ToInt32(Console.ReadLine());
 
-if(a>b)
+if(number1>number2)
 {
     Console.Write("Max = ");
-    Console.WriteLine(a);
+    Console.WriteLine(number1);
     Console.Write("Min = ");
-    Console.Write(b);
+    Console.Write(number2);
 }
 
 else 
 {
     Console.Write("Max = ");
-    Console.WriteLine(b);
+    Console.WriteLine(number1);
     Console.Write("Min = ");
-    Console.Write(a); 
+    Console.Write(number2); 
 }
