1125aa3741ad154bc2ba6ef2c016ed62e32072c9
SergeiPospelov <pospelka413@gmail.com>
2022-03-07 12:22:33 +0300

    Added Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int cartesianQuadrant = Convert.ToInt32(Console.ReadLine());
+
+if (cartesianQuadrant == 1)
+{
+    Console.WriteLine("X от 0 до +Бесконечности; Y от 0 до +Бесконечности");
+}
+else if (cartesianQuadrant == 2)
+{
+    Console.WriteLine("X от 0 до -Бесконечности; Y от 0 до +Бесконечности");
+}
+
+else if (cartesianQuadrant == 3)
+{
+    Console.WriteLine("X от 0 до -Бесконечности; Y от 0 до -Бесконечности");
+}
+
+else if (cartesianQuadrant == 4)
+{
+    Console.WriteLine("X от 0 до +Бесконечности; Y от 0 до -Бесконечности");
+}
+else
+{
+    Console.WriteLine("ZERO");
+}
