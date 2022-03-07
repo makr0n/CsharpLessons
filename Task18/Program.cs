35f0a147b12786f8d13598d095bf3b46710bff5e
SergeiPospelov <pospelka413@gmail.com>
2022-03-07 12:37:22 +0300

    Switch case solution added to Task18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,7 +1,27 @@
 ﻿Console.WriteLine("Введите номер четверти: ");
 int cartesianQuadrant = Convert.ToInt32(Console.ReadLine());
+switch (cartesianQuadrant)
+{
+    case (1):
+        Console.WriteLine("X от 0 до +Бесконечности; Y от 0 до +Бесконечности");
+        break;
 
-if (cartesianQuadrant == 1)
+    case (2):
+        Console.WriteLine("X от 0 до -Бесконечности; Y от 0 до +Бесконечности");
+        break;
+
+    case (3):
+        Console.WriteLine("X от 0 до -Бесконечности; Y от 0 до -Бесконечности");
+        break;
+
+    case (4):
+        Console.WriteLine("X от 0 до +Бесконечности; Y от 0 до -Бесконечности");
+        break;
+
+}
+
+
+/*if (cartesianQuadrant == 1)
 {
     Console.WriteLine("X от 0 до +Бесконечности; Y от 0 до +Бесконечности");
 }
@@ -23,3 +43,4 @@ else
 {
     Console.WriteLine("ZERO");
 }
+*/
