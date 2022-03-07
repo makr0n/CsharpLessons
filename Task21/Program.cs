762904ceec6358d5791e6ecaaedf11e7a45a5f92
SergeiPospelov <pospelka413@gmail.com>
2022-03-07 13:02:32 +0300

    Added task21

---
+++ Task21/Program.cs
@@ -0,0 +1,13 @@
+﻿double VectorLength= new double();
+
+Console.WriteLine("Введите координаты точки А");
+int XCoordinateOfA = Convert.ToInt32(Console.ReadLine());
+int YCoordinateOfA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B");
+int XCoordinateOfB = Convert.ToInt32(Console.ReadLine());
+int YCoordinateOfB = Convert.ToInt32(Console.ReadLine());
+
+VectorLength=Math.Sqrt((XCoordinateOfB-XCoordinateOfA)*(XCoordinateOfB-XCoordinateOfA)+(YCoordinateOfB-YCoordinateOfA)*(YCoordinateOfB-YCoordinateOfA)); //формула длины вектора
+Console.WriteLine(VectorLength);
+
