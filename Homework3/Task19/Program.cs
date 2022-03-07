443b2de6e219d59f76ec6dd0608c07af07d7c47d
SergeiPospelov <pospelka413@gmail.com>
2022-03-07 14:51:59 +0300

    Added Task19

---
+++ Homework3/Task19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int oldValue = number;
+int newValue = 0;
+
+while (number>0)
+{
+    int digit = number%10;
+    newValue=newValue*10+digit;
+    number=number/10;
+}
+
+if (newValue == oldValue)
+{
+Console.Write($"Число {oldValue} является палиндромом.");
+}
+
+else
+{
+ Console.Write($"Число {oldValue} не является палиндромом.");   
+}
