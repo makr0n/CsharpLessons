c9054761e17cb1ebcdf9f1909212d94dd47767c0
SergeiPospelov <pospelka413@gmail.com>
2022-03-07 13:29:01 +0300

    Task22 added

---
+++ Task22/Program.cs
@@ -0,0 +1,10 @@
+﻿string NStr = Console.ReadLine();
+int N = Convert.ToInt32(NStr);
+
+for (int i = 1; i <= N; i ++)
+{
+    int square=i*i;
+    Console.Write(square + " ");
+}
+
+Console.WriteLine();
