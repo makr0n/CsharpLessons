4aed8ca85101076d618e744386105fa88f89c90d
SergeiPospelov <pospelka413@gmail.com>
2022-03-07 14:07:40 +0300

    Added Task23

---
+++ Homework3/Task23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i ++)
+{
+    int cubeNumber=i*i*i;
+    Console.Write(cubeNumber + " ");
+}
+
+Console.WriteLine();
