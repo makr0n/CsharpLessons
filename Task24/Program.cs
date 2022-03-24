96ab03bfb6f6cff4fc1211840876d11d6e0b521d
SergeiPospelov <pospelka413@gmail.com>
2022-03-24 16:50:41 +0300

    Added Task28

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -9,7 +9,7 @@ int sumFromOneToNumber(int number)
     int sum = 0;
     for (int i = 0; i <= number; i++)
     {
-        sum += i;
+        sum += i;// присваивание сложения sum=sum+1
     }
     return sum;
 }
