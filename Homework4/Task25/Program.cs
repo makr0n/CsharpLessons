dd38c9bf779127ff1459d55a2d3b0293b87646a0
SergeiPospelov <pospelka413@gmail.com>
2022-03-26 20:17:32 +0300

    added Task25

--- Homework4/Task25/Program.cs
+++ Homework4/Task25/Program.cs
@@ -1,3 +1,21 @@
 ﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 3, 5 -> 243 (3⁵)
 2, 4 -> 16*/
+
+int PowerOfNumber(int number, int power)
+{
+    int result = 1;
+    for (int i = 1; i <= power; i++)
+    {
+        result = result * number;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int power = Convert.ToInt32(Console.ReadLine());
+
+int poweringResult=PowerOfNumber(number, power);
+Console.Write($"Результатом возведения {number} в степень {power} является {poweringResult}");
