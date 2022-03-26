a77d0595aa32f83ae2d8c35692103d6da76f1f59
SergeiPospelov <pospelka413@gmail.com>
2022-03-26 17:05:15 +0300

    Added Task32

---
+++ Task32/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 32: Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+*/
+
+int[] GetArrayOfNumber(int length, int deviation)
+{
+    int[] arrayOfNumbers = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arrayOfNumbers[i] = new Random().Next(-deviation, deviation);
+    }
+    return arrayOfNumbers;
+}
+
+int[] reverseDigits(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+int[] arrayOfNumbers = GetArrayOfNumber(4,9);
+PrintArray(arrayOfNumbers);
+Console.WriteLine();
+reverseDigits(arrayOfNumbers);
+PrintArray(arrayOfNumbers);
