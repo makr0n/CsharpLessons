06efbd6378e4557109da39297e0c23968cf718e1
SergeiPospelov <pospelka413@gmail.com>
2022-03-26 17:08:25 +0300

    Added Task33

---
+++ Task33/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да
+*/
+
+bool IsNumberContains(int[] array, int Number)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]==Number)
+        {
+            return true;
+        }
+        
+    }
+    return false;
+}
+
+int[] exampleArray = { 6, 7, 19, 345, 3 };
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool answer = IsNumberContains(exampleArray,number);
+
+if (answer)
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
+
