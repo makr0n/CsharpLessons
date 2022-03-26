1993a67b5fc1a951436ce6dc69bab4ce65a3fca9
SergeiPospelov <pospelka413@gmail.com>
2022-03-26 14:32:44 +0300

    changed folders

---
+++ Practice_lessons/Task32/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+*/
+
+int[] arrayOfNumbers = { -4, -8, 8, 2 };
+
+int[] reverseDigits(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+        
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+PrintArray(arrayOfNumbers);
+Console.WriteLine();
+reverseDigits(arrayOfNumbers);
+PrintArray(arrayOfNumbers);
---
+++ Practice_lessons/Task8_2/Program.cs
@@ -0,0 +1,9 @@
+﻿string NStr = Console.ReadLine();
+int N = Convert.ToInt32(NStr);
+
+for (int i = 2; i < N; i += 2)
+{
+    Console.Write(i + " ");
+}
+
+Console.WriteLine();
