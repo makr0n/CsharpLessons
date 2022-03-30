4fd0ee5d9c093b8c8ae2e949920516a829cf1ffb
SergeiPospelov <pospelka413@gmail.com>
2022-03-30 20:51:31 +0300

    Added Task36

---
+++ Homework5/Task36/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+
+int[] GetArrayOfRandomNumbers(int length, int deviation)
+{
+    int[] arrayOfNumbers = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arrayOfNumbers[i] = new Random().Next(-deviation, deviation + 1);
+    }
+    return arrayOfNumbers;
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        if (i < count - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int SumOfElementsInOddPositions(int[] array)
+{
+    int sumOfNumbersInOddPositions = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 0)
+        {
+            sumOfNumbersInOddPositions = sumOfNumbersInOddPositions + array[i];
+        }
+    }
+    return sumOfNumbersInOddPositions;
+}
+
+
+int[] randomArray = GetArrayOfRandomNumbers(4, 10);
+PrintArray(randomArray);
+int sumOfNumbersInOddPositions = SumOfElementsInOddPositions(randomArray);
+Console.Write($"Сумма элементов на нечётных позициях равна {sumOfNumbersInOddPositions}.");
