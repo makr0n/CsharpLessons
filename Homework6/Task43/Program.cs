f5ba5b0586368b85198f11bc1a52bf9cfe74bc48
SergeiPospelov <pospelka413@gmail.com>
2022-04-03 19:58:44 +0300

    Changed variable type to double in Task43

--- Homework6/Task43/Program.cs
+++ Homework6/Task43/Program.cs
@@ -4,19 +4,22 @@ b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
 
 
 Console.Write("Введите коэффициент k1 для первого уравнения: ");
-int coefficientK1 = Convert.ToInt32(Console.ReadLine());
+ double coefficientK1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите коэффициент b1 для первого уравнения: ");
-int coefficientB1 = Convert.ToInt32(Console.ReadLine());
+double coefficientB1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите коэффициент k2 для второго уравнения: ");
-int coefficientK2 = Convert.ToInt32(Console.ReadLine());
+double coefficientK2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите коэффициент b2 для первого уравнения: ");
-int coefficientB2 = Convert.ToInt32(Console.ReadLine());
+double coefficientB2 = Convert.ToInt32(Console.ReadLine());
 
 if (coefficientK1 != coefficientK2)
 {
     double firstCoordinate = (coefficientB2 - coefficientB1) / (coefficientK1 - coefficientK2);
+    Console.WriteLine(firstCoordinate);
+    
     double secondCoordinate = coefficientK1 * firstCoordinate + coefficientB1;
+    Console.WriteLine(secondCoordinate);
 
     Console.WriteLine($"Координаты точки пересечения прямых ({firstCoordinate}; {secondCoordinate})");
 }
